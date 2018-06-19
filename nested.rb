1e0f0787212a66b9c7b29584fb2350b49ff902ee
Toni Lamont <tlam212@gmail.com>
2018-06-19 22:47:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,8 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     } 
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -79,7 +81,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
     
 end
 
@@ -101,7 +102,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
 end
 
@@ -121,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] =  "Assembly"
+programmer_hash
 end
