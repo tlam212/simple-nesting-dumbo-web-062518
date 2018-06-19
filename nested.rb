c438255865674d843619cf4c8ac11719c8d0cc07
Toni Lamont <tlam212@gmail.com>
2018-06-19 22:48:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -123,6 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] =  "Assembly"
+programmer_hash[:dennis_ritchie][:languages] = ["C"].push("assembly")
 programmer_hash
 end
